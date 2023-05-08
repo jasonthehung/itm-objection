281a26c1a53cf772d43f461fd2e4fcfc1ad26d18
Jason Wang <wanghung07@gmail.com>
2023-05-08 14:05:42 +0800

    .

--- deploy/02-deploy-ecRecover.ts
+++ deploy/02-deploy-ecRecover.ts
@@ -1,4 +1,15 @@
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+
+  const ecRecover = await deploy("ecRecover", {
+    from: deployer,
+    args: [],
+    log: true,
+    waitConfirmations: 1,
+  });
+
+  log("----------------------------------------------------------------");
 };
+
+module.exports.tags = ["all", "ecRecover"];
